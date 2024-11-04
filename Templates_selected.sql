4594ba0d91cd05f9cfad9c520e5330c282edba04
Svetlana L <151991234+Svetlless@users.noreply.github.com>
2024-11-04 20:08:00 +0700

    Update Templates_selected.sql

--- Templates_selected.sql
+++ Templates_selected.sql
@@ -1,22 +1,22 @@
-/*1. Дана таблица действий пользователей:
+/*1. The following data of the photo editor app are given:
 -ID,
 -time,
--event - тип действия (в т.ч. template_selected),
--event_type (конкретизация действия, например, название шаблона).
-Необходимо вывести 10 шаблонов, которые пользователи применяли 2 и более раз подряд в течение одной сессии.
-Сессия - последовательность событий, в которой промежуток между соседними событиями не более 3 минут
-(т.е. длина самой сессии может быть любой, но между последовательными событиями интервал не более 3 минут)*/
+-event - type of event (e.g. template_selected),
+-event_type (e.g. template title).
+The task is to create an output with 10 templates that the users selected two or more times in a row during one session.
+Session is a sequence of events in which the interval between the events is no more than 3 minutes
+(i.e. the length of the session itself can be longer than 3 minutes, but the interval between the consecutive events shouldn't be more than 3 minutes)*/
 
 WITH EventsLen AS (
          SELECT ID,
-                time::timestamp AS time,--неизвестно, какой формат time, поэтому на всякий случай изменила на timestamp
+                time::timestamp AS time,
                 time::timestamp - LAG(time::timestamp) OVER (
                                  PARTITION BY user_id
                                      ORDER BY time) AS events_len,
                 event,
                 event_type
            FROM table),
--- в этом CTE вычисляем разницу во времени между последовательными событиями каждого юзера
+-- in this CTE I calculate the difference between the consecutive events of each user
      SessionLabel AS (
          SELECT ID,
                 time,
@@ -26,7 +26,7 @@ WITH EventsLen AS (
                 event,
                 event_type
            FROM EventsLen),
---в этом CTE присваиваем 1 всем событиям, которые произошли с интервалом более 3 минут
+-- in this CTE I mark all users with the interval more than 3 minutes as 1 
      SessionID AS (
          SELECT ID,
                 time,
@@ -34,15 +34,15 @@ WITH EventsLen AS (
                 event,
                 event_type
            FROM SessionLabel),
--- в этом CTE суммируем 0 и 1 как накопительную сумму, чтобы для каждого юзера получить номер сессии
+-- in this CTE I sum up 0 and 1 as the cumulative sum, in order to obtain a session number for each user
      PrevTemplate AS (
          SELECT ID,
                 session_id,
                 event_type,
-			 LAG(event_type) OVER (PARTITION BY ID, session_id ORDER BY time) AS prev_template
+		LAG(event_type) OVER (PARTITION BY ID, session_id ORDER BY time) AS prev_template
            FROM SessionID
           WHERE event='template_selected'),
--- для каждой сессии каждого юзера получаем значение предыдущего шаблона, если его нет (это – первое действие), оставляем значение текущего шаблона
+-- for each session of each user I get the name of the previous template used
      Counts AS (
          SELECT ID,
                 session_id,
@@ -51,10 +51,10 @@ WITH EventsLen AS (
            FROM PrevTemplate
           WHERE template=prev_template
        GROUP BY ID, session_id, template)
--- суммируем все строки, сгруппированные по юзерам, сессиям и типам шаблона, когда текущий шаблон = предыдущему (т.е., шаблоны выбраны подряд)
+-- I sum up all rows grouped by users, sessions and template types when current template = previous template
   SELECT template,
          template_number
     FROM Counts
-   WHERE template_number >= 1 #т.к. если шаблон=предыдущему шаблону, его выбрали 2 раза и более
+   WHERE template_number >= 1 -- if current template=previous template, it means that the template was used two or more times in a row
 ORDER BY template_number DESC
    LIMIT 10
